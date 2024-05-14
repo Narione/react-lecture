d9f89399591d90d0263433c3af87e5e3f92ccc20
Narione <ynhp3214@gmail.com>
2024-05-14 16:58:04 +0900

    240514 hookUseEffect

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import HookUseState from "./lecture/HookUseState";
 import JsxRolues from "./lecture/jsxRolues";
 import JsxRolues_m from "./mission/jsxRolues";
 import HookUseState_m from "./mission/HookUseState";
+import HookUseEffect from "./lecture/HookUseEffect";
+import HookUseEffect_m from "./mission/HookUseEffect";
 function App() {
   return (
     <div className="App">
       {/* 설명 컴포넌트 */}
       {/* <JsxRolues /> */}
       {/* <HookUseState /> */}
-      <HookUseState_m />
+      {/* <HookUseEffect /> */}
 
       {/* 미션 컴포넌트 */}
       {/* <JsxRolues_m /> */}
+      {/* <HookUseState_m /> */}
+      <HookUseEffect_m />
     </div>
   );
 }
